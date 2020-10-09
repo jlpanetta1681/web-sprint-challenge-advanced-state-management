286d0ad3a32f421e8230e6ede4c2205a06facd95
Joe-Panetta <jlpanetta1681@icloud.com>
2020-10-09 12:17:49 -0400

    complete but having a render error

---
+++ smurfs/src/components/List.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import Smurf from '../store/actions/Smurf';
+import { connect } from 'react-redux';
+import { fetchSmurfs } from '../store/actions/index.js';
+
+const List = (props) => {
+	useEffect((props) => {
+		props.fetchSmurfs();
+	}, []);
+
+	return (
+		<div className="List">
+			<h3>Smurf List</h3>
+			{props.isLoading && <h3>Loading smurf data...</h3>}
+			{props.error && <p className="error">{props.error}</p>}
+			{props.smurfs.length > 0 &&
+				props.smurfs.map((smurf) => (
+					<Smurf key={smurf.id} smurf={smurf} />
+				))}
+		</div>
+	);
+};
+
+const mapStateToProps = (state) => {
+	return {
+		isLoading: state.isLoading,
+		smurfs: state.smurfs,
+		error: state.error,
+	};
+};
+
+export default connect(mapStateToProps, { fetchSmurfs })(List);
